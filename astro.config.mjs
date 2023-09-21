55604a4a4f4a81bcab632b5a541777c7c40b82f3
gonzlezjg <15gonzalez.j.p@gmail.com>
2023-09-21 18:02:13 -0400

    deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,10 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import { SITE_URL } from './src/data/config';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: SITE_URL,
-  output: 'server',
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
@@ -17,8 +14,5 @@ export default defineConfig({
       wrap: false,
     },
   },
-  adapter: node({
-    mode: 'standalone',
-  }),
   site: 'https://Gonzlezjg.github.io',
 });
